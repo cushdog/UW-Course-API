e06db7d4b0e1982cd53f24a1076d7ed20c5181a5
cushdog <tylercush03@gmail.com>
2024-08-19 23:03:22 -0500

    Made a MUCH better database

--- app.py
+++ app.py
@@ -7,19 +7,14 @@ import os
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://course-explorer-electric-boogaloo.vercel.app"]}})
 
-DATABASE_CATALOG = 'catalog_courses.db'
-DATABASE_SECTIONS = 'sections.db'
+DATABASE = 'courses.db'
 
-def get_catalog_db_connection():
-    conn = sqlite3.connect(DATABASE_CATALOG)
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
     return conn
 
-def get_sections_db_connection():
-    conn = sqlite3.connect(DATABASE_SECTIONS)
-    return conn
-
-def pull_from_table_catalog(course_title):
-    conn = get_catalog_db_connection()
+def pull_from_table(course_title):
+    conn = get_db_connection()
     cursor = conn.cursor()
     query = "SELECT * FROM courses WHERE course_code = ?;"
     cursor.execute(query, (course_title,))
@@ -28,17 +23,6 @@ def pull_from_table_catalog(course_title):
     if not results:
         return "Course not found"
     return results[0]
-
-def pull_from_table_sections(course_title):
-    conn = get_sections_db_connection()
-    cursor = conn.cursor()
-    query = "SELECT * FROM courses WHERE course_name LIKE ?;"
-    cursor.execute(query, ('%' + course_title + '%',))
-    results = cursor.fetchall()
-    conn.close()
-    if not results:
-        return "Course not found"
-    return results
 
 @app.route('/search', methods=['GET'])
 def search():
@@ -49,13 +33,9 @@ def search():
     class_name, course_number = words[0].upper(), words[1]
     course_title = class_name + " " + course_number
 
-    catalog_results = jsonify(pull_from_table_catalog(course_title))
-    sections_results = jsonify(pull_from_table_sections(course_title))
+    results = pull_from_table(course_title)
 
-    return jsonify({
-        "catalog": catalog_results,
-        "sections": sections_results
-    })
+    return jsonify(results)
 
 
 if __name__ == '__main__':
