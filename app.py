38dce69fb313e45a3adf43f1b3fad07e04d7693a
cushdog <tylercush03@gmail.com>
2024-08-19 23:15:34 -0500

    Made a MUCH better database

--- app.py
+++ app.py
@@ -13,11 +13,22 @@ def get_db_connection():
     conn = sqlite3.connect(DATABASE)
     return conn
 
-def pull_from_table(course_title):
+def course_pull_from_table(course_title):
     conn = get_db_connection()
     cursor = conn.cursor()
     query = "SELECT * FROM courses WHERE course_code = ?;"
     cursor.execute(query, (course_title,))
+    results = cursor.fetchall()
+    conn.close()
+    if not results:
+        return "Course not found"
+    return results[0]
+
+def subject_pull_from_table(subject):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    query = "SELECT * FROM courses WHERE subject = ?;"
+    cursor.execute(query, (subject,))
     results = cursor.fetchall()
     conn.close()
     if not results:
@@ -33,7 +44,19 @@ def search():
     class_name, course_number = words[0].upper(), words[1]
     course_title = class_name + " " + course_number
 
-    results = pull_from_table(course_title)
+    results = course_pull_from_table(course_title)
+
+    return jsonify(results)
+
+@app.route('/subject-search', methods=['GET'])
+def search():
+
+    query = request.args.get('query')
+    words = query.split()
+    
+    subject = words[0].upper()
+
+    results = subject_pull_from_table(subject)
 
     return jsonify(results)
 
